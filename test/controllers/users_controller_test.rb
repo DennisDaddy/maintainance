676c6f3dafd62236e39be402c7f0d149b28157c3
DennisDaddy <softteez95@gmail.com>
2016-08-01 02:00:15 +0300

    test index action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user =user(:michael)
+    @other_user(:archer)
+  end
   test "should get new" do
     get users_new_url
     assert_response :success
