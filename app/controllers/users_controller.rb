f71af47938e083d28377c91e3f38fcdd4651ba2d
DennisDaddy <softteez95@gmail.com>
2016-08-03 11:06:39 +0300

    complete showing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
 	def show
      @user = User.find(params[:id])
     end
- 
 
+    def index
+      @users =User.all
+
+    end
+ 
   def new
   	@user =User.new
   end
 
-  def create
+ def create
   	@user = User.new(user_params)
   	if @user.save
       log_in @user
@@ -19,9 +23,9 @@ class UsersController < ApplicationController
   	redirect_to @user
 
   	else
-  		render 'new'
+  render 'new'
 
-  	end
+ end
   end
 
   def edit
@@ -39,9 +43,6 @@ class UsersController < ApplicationController
   end
 end
 
-
-
-
   private
 
   def user_params
@@ -52,11 +53,12 @@ end
       store_location
       flash[:danger] ="please login"
       redirect_to login_url
-    end
+
   end
+
   def correct_user
   @user = User.find(params[:id])
   redirect_to(root_url) unless @user == current_user?(@user)
 end
 end
-
+end
