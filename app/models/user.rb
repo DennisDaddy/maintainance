6cd9894af530370897f7c77f517a429618463e3d
DennisDaddy <softteez95@gmail.com>
2016-08-04 11:57:31 +0300

    complete account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token, :activation_token
-    before_save :downcase_email
-    before_create :create_activation_digest
+        before_save :downcase_email
+        before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
@@ -10,10 +10,10 @@ class User < ApplicationRecord
 	validates :password, length: { minimum: 6 }, allow_blank: true
 
 	def User.digest(string)
-cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-BCrypt::Engine.cost
-BCrypt::Password.create(string, cost: cost)
-end
+     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+     BCrypt::Engine.cost
+     BCrypt::Password.create(string, cost: cost)
+   end
 
   def User.new_token
    SecureRandom.urlsafe_base64
@@ -25,14 +25,28 @@ end
   	
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  	
-  end
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+
+   digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+
+   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Activates an account.
+  def activate
+   update_attribute(:activated, true)
+   update_attribute(:activated_at, Time.zone.now)
+ end
+
+  # Sends activation email.
+  def send_activation_email
+   UserMailer.account_activation(self).deliver_now
   end
 
  
